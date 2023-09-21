2803a642faf2c3ed180f25704f8693911c6400c0
venkatnandi <venkatnandi09@gmail.com>
2023-09-21 18:26:54 +0530

    first commit

---
+++ app_server/controllers/list.js
@@ -0,0 +1,36 @@
+/* Home page */
+module.exports.homelist = function(req,res){
+    res.render('songs-list', {title : "Home"});
+};
+
+/* Songs page */
+module.exports.songsInfo = function(req,res){
+    res.render('songs-info', {title : "Songs Information"});
+};
+
+module.exports.songsInfo1 = function(req,res){
+    res.render('songs-info1', {title : "Songs Information"});
+};
+
+module.exports.catalogue= function(req,res){
+    res.render('songs-catalogue', {title : "Songs Catalogue"});
+};
+
+module.exports.songsInfo2= function(req,res){
+    res.render('songs-info2', {title : "Songs Catalogue"});
+};
+
+
+module.exports.songsInfo3= function(req,res){
+    res.render('songs-info3', {title : "Songs Catalogue"});
+};
+
+module.exports.songsInfo4= function(req,res){
+    res.render('songs-info4', {title : "Songs Catalogue"});
+};
+
+/* Review page */
+module.exports.addReview = function(req,res){
+    res.render('songs-review-form', {title : "Add Review"});
+};
+
