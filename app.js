1b67ef74170c720f4af4367e4f540e06ad3b58a4
venkatnandi <venkatnandi09@gmail.com>
2023-10-03 13:05:05 +0530

    Mongoose connected

--- app.js
+++ app.js
@@ -3,15 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('./app_api/models/db');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
+var routesApi = require('./app_api/routes/index');
 
 var app = express();
-
-// mongodb connection
-require('./models/connect');
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
@@ -25,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', routesApi)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
